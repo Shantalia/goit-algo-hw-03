a40230cab5b8c8017bc40ee928cd57dcd310fa35
Kate Kozachenko <kottya1995@ukr.net>
2024-01-28 22:21:09 +0100

    Add files via upload

---
+++ Task1.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+import re
+
+
+def get_days_from_today(date:str): # формат РРРР-ММ-ДД
+    pattern = r'\d{4}\-\d{2}\-\d{2}$'
+    if bool(re.match(pattern, date)):
+        date = datetime.strptime(date, "%Y-%m-%d")
+        today = datetime.today()
+        difference = today - date
+    else: 
+        print("Невірний формат!")
+        get_days_from_today(input("Введіть дату в форматі РРРР-ММ-ДД: "))
+    return print(f"{int(difference.days)}") 
+
+while True:
+    try: 
+        get_days_from_today(input("Введіть дату в форматі РРРР-ММ-ДД: "))
+        break
+    except ValueError:
+        print('Невірна або неіснуюча дата!')
+    continue 
