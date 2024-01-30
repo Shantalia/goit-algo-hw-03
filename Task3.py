889254fd8cbaea01ef2de181e58cfc6fca2c1869
Kate Kozachenko <kottya1995@ukr.net>
2024-01-30 11:47:26 +0100

    fix Task3

--- Task3.py
+++ Task3.py
@@ -2,17 +2,26 @@ import re
 
 def normalize_phone(phone_number):
     code = "+38"
-    our_digits = r"\d+"
+    our_digits = r"[+]?\d+"
     clear_number = []
     for ch in re.findall(our_digits, phone_number):
         clear_number.append(ch)
-    string = ""
-    for i in code:
-        string += i
-        if phone_number.startswith(string):
-            continue
-        else:
-            code = string
+    phone_number = ""    
+    phone_number = phone_number.join(clear_number)
+    length = len(phone_number)
+    if length > 10:
+        number_without_code =  phone_number[-10:]
+        clear_number = number_without_code
+        string = ""
+        for i in code:
+            string += i
+            if phone_number.startswith(string):
+                break
+            else:
+                code = string
+            phone_number = ""
+            phone_number = code + phone_number.join(clear_number)
+    else:
         phone_number = ""
         phone_number = code + phone_number.join(clear_number)
     return f"Ваш номер телефону: {phone_number}"
